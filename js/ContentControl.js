bf087cc86c2768a3dc67ecd20f453aca05b65448
Chiemeka Obasi <emiy2k2@gmail.com>
2018-03-01 13:23:32 +0100

    Support Section & Estimate Aside Update

--- js/ContentControl.js
+++ js/ContentControl.js
@@ -22,8 +22,8 @@ $('.navbar-collapse ul li a').click(function() {
 //Appends content on front-page
 $(".intro-lead-in").append("<h1>APIs AS A SERVICE</h1>");
 $(".intro-heading").append("Schedule Jobs/SMS for delivery, for as low as ₦ 1.89 per unit. Get 50 SMS Units per month for every monthly subscription on Transactional SMS.");
-$(".tsms").append("<strong>Transactional SMS Job Scheduler</strong><br> to send messages to any mobile number in Nigeria. We provide a service-facing API that can be used to execute timely SMSs on your running business platforms.");
-$(".recurrent").append("<strong>Recurrent Job Scheduler</strong><br> to schedule jobs that recurrently execute and be automatically re-scheduled. We provide an API to execute such recurrent jobs as required. (...coming soon)");
-$(".cron").append("<strong>Cron Job Scheduler</strong><br> that can be used for jobs intended to run periodically at fixed times, dates or intervals. (...coming soon)");
-$(".one-time").append("<strong>One-Time Job Scheduler</strong><br> that can be used to schedule jobs intended to run only once. (...coming soon)");
-$(".bitcoin").append("<strong>Bitcoin Transaction Confirmation</strong><br> service can be used to confirm all your Bitcoin transctions in real-time. (...coming soon)");
+$(".tsms").append("<strong>Transactional SMS Job Scheduler</strong><br>Send messages to any mobile number in Nigeria. We provide a service-facing API that can be used to execute timely SMSs on your running business platforms.");
+$(".recurrent").append("<strong>Recurrent Job Scheduler</strong><br> Schedule jobs that recurrently execute and  can be automatically re-scheduled. We provide a service API to execute such recurrent jobs as required.");
+$(".cron").append("<strong>Cron Job Scheduler</strong><br> Schedule jobs intended to run periodically at fixed times, dates or intervals. We have a service API available to get you up and running.");
+$(".one-time").append("<strong>One-Time Job Scheduler</strong><br> Schedule jobs intended to run only once. Simply setup and enable using our service API.");
+$(".bitcoin").append("<strong>Bitcoin Transaction Confirmation</strong><br> Setup and enbale real-time confirmation of all your Bitcoin transctions. It's that easy!");
