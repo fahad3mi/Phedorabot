715ffb9b15fd5e899e4c20dde45293ac49c3329b
Chiemeka Obasi <emiy2k2@gmail.com>
2018-03-23 09:36:44 +0100

    header mobile height

--- js/ContentControl.js
+++ js/ContentControl.js
@@ -19,26 +19,25 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+var introHeadings =["Automate Tasks for your Digital Solutions",
+                    "Schedule Transactional Triggers",
+                    "Provide reliable Customer Support Channels"];
+
+                    introHeadings.forEach(function(item, index, array){
+                        console.log(item, index);
+                    });
+
 //Appends content on front-page
-$(".intro-lead-in").append("<h1>APIs AS A SERVICE</h1>");
-$(".intro-heading").append("Schedule Jobs/SMS for delivery, for as low as ₦ 1.89 per unit. Get 50 SMS Units per month for every monthly subscription on Transactional SMS.");
+$(".intro-lead-in").append("<h1>API AS A SERVICE</h1>");
+$(".intro-heading").append(introHeadings[0]);
 $(".tsms").append("<strong>Transactional SMS Job Scheduler</strong><br>Send messages to any mobile number in Nigeria. We provide a service-facing API that can be used to execute timely SMSs on your running business platforms.");
 $(".recurrent").append("<strong>Recurrent Job Scheduler</strong><br> Schedule jobs that recurrently execute and  can be automatically re-scheduled. We provide a service API to execute such recurrent jobs as required.");
 $(".cron").append("<strong>Cron Job Scheduler</strong><br> Schedule jobs intended to run periodically at fixed times, dates or intervals. We have a service API available to get you up and running.");
 $(".one-time").append("<strong>One-Time Job Scheduler</strong><br> Schedule jobs intended to run only once. Simply setup and enable using our service API.");
 $(".bitcoin").append("<strong>Bitcoin Transaction Confirmation</strong><br> Setup and enbale real-time confirmation of all your Bitcoin transctions. It's that easy!");
 
+var changeIntro = function() {
+    $(".into-heading").append(introHeadings[1]);
+};
 
- //------------------------------------//
-  //Wow Animation//
-  //------------------------------------// 
- /** wow = new WOW(
-    {
-      boxClass:     'wow',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       false        // trigger animations on mobile devices (true is default)
-    }
-  );
-  wow.init();**/
-
+changeIntro();
